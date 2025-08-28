76817d301a5f457ab3881775c04c518f71476db1
zhangwenjian <zwj777@live.com>
2025-08-28 19:52:48 +0800

    test: 修复 className 测试，确保异步设置属性后正确渲染

--- tests/unit/components/SvgIcon.spec.js
+++ tests/unit/components/SvgIcon.spec.js
@@ -9,14 +9,14 @@ describe('SvgIcon.vue', () => {
     })
     expect(wrapper.find('use').attributes().href).toBe('#icon-test')
   })
-  it('className', () => {
+  it('className', async() => {
     const wrapper = shallowMount(SvgIcon, {
       propsData: {
         iconClass: 'test'
       }
     })
     expect(wrapper.classes().length).toBe(1)
-    wrapper.setProps({ className: 'test' })
-    expect(wrapper.classes().includes('test')).toBe(true)
+    await wrapper.setProps({ className: 'test' })
+    expect(wrapper.find('svg').classes().includes('test')).toBe(true)
   })
 })
