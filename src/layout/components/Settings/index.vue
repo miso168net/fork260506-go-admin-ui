1afe4ca7944799e03dca8047631112e8d7856c9a
zhangwenjian <zwj777@live.com>
2025-09-16 18:56:28 +0800

    feat: 更新ThemePicker导入路径，添加文件扩展名以确保正确加载

--- src/layout/components/Settings/index.vue
+++ src/layout/components/Settings/index.vue
@@ -67,7 +67,7 @@
 </template>
 
 <script>
-import ThemePicker from '@/components/ThemePicker'
+import ThemePicker from '@/components/ThemePicker/index.vue'
 
 export default {
   components: { ThemePicker },
